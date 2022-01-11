7ad2fa12952b6562537736809857847d633fe13e
Arne Dußin <arne.dussin@gmail.com>
2022-01-11 10:27:29 +0100

    Define main classes

--- src/class.rs
+++ src/class.rs
@@ -16,6 +16,24 @@ pub enum Class
     Spy,
 }
 
+impl Class
+{
+    /// Check if the class is considered a "main" class, which means it is under
+    /// consideration for main (most) played class during a game.
+    ///
+    /// # Returns
+    /// `true` if the class is main-classeable, currently that includes
+    /// `Demoman`, `Scout`, `Soldier` and `Medic`. `false` for all other
+    /// classes.
+    pub fn is_main_class(self) -> bool
+    {
+        match self {
+            Self::Demoman | Self::Medic | Self::Scout | Self::Soldier => true,
+            _ => false,
+        }
+    }
+}
+
 /// When creating identifying a class from a string, the class may be unknown in
 /// case the string does not conform to all lowercase string as it is present in
 /// the logs.tf API. In that case, this error is thrown, containing the content
